bc0f39d6ac92d8e7cb7872ee246c90c26298dbb8
Marcelo XP <marceloxp@gmail.com>
2020-04-01 08:27:54 -0300

    - Adjust build to root folder.

---
+++ precache-manifest.77b344ce0aba26c4b8d064d545ba9fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5604cce299ac0e45bcb7d1b2b91ccc5",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "f78d196a9dc1943a930c",
+    "url": "/page/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c59a56d4659acf5b58f6",
+    "url": "/page/static/css/main.86e86f99.chunk.css"
+  },
+  {
+    "revision": "f78d196a9dc1943a930c",
+    "url": "/page/static/js/2.4080b426.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/page/static/js/2.4080b426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59a56d4659acf5b58f6",
+    "url": "/page/static/js/main.9a892537.chunk.js"
+  },
+  {
+    "revision": "c1c512b5d44196cf69a3",
+    "url": "/page/static/js/runtime-main.618c66b4.js"
+  }
+]);
